7750669008f304c0bd4b1db7c9916ee0d15315df
Michael Fields <michael@mfields.org>
2012-02-22 21:37:14 -0800

    No need to use recursion to scan the file.

--- vip-scanner/class-preg-file.php
+++ vip-scanner/class-preg-file.php
@@ -14,8 +14,16 @@ class VIP_PregFile {
 	private $results = array();
 
 	/**
-	 * @param string $needle A Perl compatible regular expression.
+	 * Create an instance of this object.
+	 *
+	 * @uses VIP_PregFile::$pattern
+	 * @uses VIP_PregFile::$subject
+	 * @uses VIP_PregFile::$matches
+	 * @uses VIP_PregFile::scan()
+	 *
+	 * @param string $needle A Perl Compatible Regular Expression.
 	 * @param string $haystack A file to search through.
+	 * @return void
 	 */
 	public function __construct( $needle, $haystack ) {
 		$this->pattern = $needle;
@@ -30,40 +38,54 @@ class VIP_PregFile {
 		$this->subject = preg_split( '/$\R?^/m', $this->subject );
 
 		foreach ( $matches[0] as $match ) {
-			$this->scan( count( preg_split( '/$\R?^/m', $match ) ) );
-		}
-	}
-
-	public function scan( $length ) {
-		$subject = array();
-		$scanned = array();
-		for ( $i = $this->line; $i < ( $this->line + $length ); $i++ ) {
-			if ( isset( $this->subject[$i] ) ) {
-				$subject[] = $this->subject[$i];
-				$scanned[] = $i;
-			}
-		}
-
-		$subject = implode( "\n", $subject );
-
-		if ( preg_match( $this->pattern, $subject ) ) {
-			$line_number = $this->line + 1;
-			$this->results[$line_number] = $subject;
-			foreach ( $scanned as $remove ) {
-				unset( $this->subject[$remove] );
-			}
-			return;
-		}
-
-		unset( $this->subject[$this->line] );
-		$this->line++;
-
-		if ( 0 < count( $this->subject ) )
+			$length = count( preg_split( '/$\R?^/m', $match ) );
 			$this->scan( $length );
-
-		return;
+		}
 	}
 
+	/**
+	 * Find the needle in the haystack.
+	 *
+	 * Iterates through the lines in VIP_PregFile::$subject.
+	 * Sets VIP_PregFile::$results
+	 *
+	 * @uses VIP_PregFile::$subject
+	 * @uses VIP_PregFile::$line
+	 * @uses VIP_PregFile::$results
+	 *
+	 * @param int $length Number of lines that the regex match spans.
+	 * @return void
+	 */
+	public function scan( $length ) {
+		foreach ( $this->subject as $line ) {
+			$subject = array();
+			for ( $i = $this->line; $i < ( $this->line + $length ); $i++ ) {
+				if ( isset( $this->subject[$i] ) )
+					$subject[] = $this->subject[$i];
+			}
+
+			$subject = implode( "\n", $subject );
+
+			if ( preg_match( $this->pattern, $subject ) ) {
+				$line_number = $this->line + 1;
+				$this->results[$line_number] = $subject;
+				unset( $this->subject[$this->line] );
+				$this->line++;
+				break;
+			}
+
+			unset( $this->subject[$this->line] );
+			$this->line++;
+		}
+	}
+
+	/**
+	 * Get the results of the scan.
+	 *
+	 * @uses VIP_PregFile::$results
+	 *
+	 * @return array Matched lines indexed by line number.
+	 */
 	public function get() {
 		return $this->results;
 	}
