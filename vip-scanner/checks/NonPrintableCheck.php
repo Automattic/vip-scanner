26f66eb99a4b4ce09457813376b7109160d880d5
Michael Fields <michael@mfields.org>
2012-02-23 13:12:42 -0800

    s/filesname/filename

--- vip-scanner/checks/NonPrintableCheck.php
+++ vip-scanner/checks/NonPrintableCheck.php
@@ -4,14 +4,14 @@ class NonPrintableCheck extends BaseCheck {
 		$result = true;
 
 		foreach ( $this->filter_files( $files, 'php' ) as $path => $content) {
-			
+
 			$this->increment_check_count();
-			
+
 			// 09 = tab
 			// 0A = line feed
 			// 0D = new line
 			$pattern = '/[\x00-\x08\x0B-\x0C\x0E-\x1F\x80-\xFF]/';
-			
+
 			if ( preg_match( $pattern, $content, $matches ) ) {
 				$filename = $this->get_filename( $path );
 				$non_print = $this->preg_content( $pattern, $content );
@@ -19,7 +19,7 @@ class NonPrintableCheck extends BaseCheck {
 					'non-printable',
 					'Non-printable characters were found in the file. You may want to check this file for errors.',
 					'info',
-					$filesname,
+					$filename,
 					$non_print
 				);
 			}
