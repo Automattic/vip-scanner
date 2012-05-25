90f164b272c64dc737cc02208e4a40853c33e033
Mohammad Jangda <batmoo@gmail.com>
2012-05-24 21:56:10 -0300

    Use `dirname( __FILE__ )` instead of `__DIR__` for PHP 5.2 compat (which is the requirement for WordPress 3.3)

--- vip-scanner-wpcom.php
+++ vip-scanner-wpcom.php
@@ -11,8 +11,8 @@ add_action( 'vip_scanner_loaded', 'vip_scanner_custom_load_rules' );
 
 function vip_scanner_custom_load_rules() {
 	VIP_Scanner::get_instance()->register_review( 'VIP Theme Review', array(
-		'VIPWhitelistCheck' => __DIR__ . '/vip-scanner-wpcom/checks/VIPWhitelistCheck.php',
-		'VIPRestrictedPatternsCheck' => __DIR__ . '/vip-scanner-wpcom/checks/VIPRestrictedPatternsCheck.php',
-		'VIPRestrictedCommandsCheck' => __DIR__ . '/vip-scanner-wpcom/checks/VIPRestrictedCommandsCheck.php',
+		'VIPWhitelistCheck' => dirname( __FILE__ ) . '/vip-scanner-wpcom/checks/VIPWhitelistCheck.php',
+		'VIPRestrictedPatternsCheck' => dirname( __FILE__ ) . '/vip-scanner-wpcom/checks/VIPRestrictedPatternsCheck.php',
+		'VIPRestrictedCommandsCheck' => dirname( __FILE__ ) . '/vip-scanner-wpcom/checks/VIPRestrictedCommandsCheck.php',
 	) );
 }
