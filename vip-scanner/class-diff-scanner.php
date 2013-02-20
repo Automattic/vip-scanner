248bb9eb59fec58bdd146721b244a160f0dfbbbb
Mohammad Jangda <batmoo@gmail.com>
2013-02-20 05:27:37 -0500

    Working version of DiffScanner.
    Adds actual code to parse a unified diff and be able to scan the included files. Only new or changed lines are scanned; deleted lines are ignored.
    
    Parsing code written by Thorsten Ott.

--- vip-scanner/class-diff-scanner.php
+++ vip-scanner/class-diff-scanner.php
@@ -8,12 +8,60 @@ class DiffScanner extends BaseScanner
 	 */
 	function __construct( $diff, $checks ) {
 
-		$diffs = preg_split( "/^(Index: .+)$/m", $diff, -1, PREG_SPLIT_DELIM_CAPTURE );
+		$diff_lines = explode( "\n", $diff );
+		$file_name = NULL;
 
-		foreach( $diffs as $diff ) {
-			// TODO
+		// Parse diff format into a nice array with line numbers
+		// props Thorsten Ott
+		foreach( $diff_lines as $line ) {
+			switch( true ) {
+				case preg_match( '/^No differences encountered/', $line ):
+				case preg_match( '/^$/', $line ):
+				case preg_match( '/^(\-\-\-|\+\+\+)/', $line ):
+					break;
+				case preg_match( '/^@@ [-+]([0-9]+)*,([0-9]+)* [+-]([0-9]+)*,([0-9]+)* @@/', $line, $match ):
+					$old_start = $match[1];
+					$new_start = $match[3];
+					$length = strlen( $new_start ) + 1;
+					if ( $length <= 4 )
+						$length = 4;
+					break;
+				case preg_match( '/^ (.*)/', $line, $match ):
+					$old_start++;
+					$new_start++;
+					$diff_split[$file_name][$new_start] = $match[1];
+					break;
+				case preg_match( '/^\+(.*)/', $line, $match ):
+					$new_start++;
+					$diff_split[$file_name][$new_start] = $match[1];
+					break;
+				case preg_match( '/^\-(.*)/', $line, $match ):
+					$old_start++;
+					break;
+				case preg_match( '/^Index: (.+)/', $line, $match ):
+					$file_name = $match[1];
+					break;
+				case preg_match( '/^diff -r/', $line ):
+					break;
+			}
 		}
 
-		parent::__construct( array( sprintf( 'file.%s', $content_type ) => $content ), $checks );
+		$diff_files = array();
+		foreach ( $diff_split as $diff_file => $diff_entry ) {
+			$i = 0;
+			$diff_entry_merged = '';
+
+			foreach ( $diff_entry as $diff_entry_line_number => $diff_entry_line ) {
+				$i++;
+				while ( $i < $diff_entry_line_number ) {
+					$diff_entry_merged .= PHP_EOL; // line number $i
+					$i++;
+				}
+				$diff_entry_merged .= $diff_entry_line . PHP_EOL; // line number $diff_entry_line_number
+			}
+			$diff_files[ $diff_file ] = $diff_entry_merged;
+		}
+
+		parent::__construct( $diff_files, $checks );
 	}
 }
