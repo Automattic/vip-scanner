053878a9a0c6c98168b7028ced7b4e3a5f1c4f0c
Nick Daugherty <ndaugherty987@gmail.com>
2013-10-31 15:32:47 -0600

    Add Check for presence of vip-init.php

---
+++ vip-scanner-wpcom/checks/VIPInitCheck.php
@@ -0,0 +1,49 @@
+<?php
+
+/**
+ * Ensure that vip-init.php is required in the theme
+ */
+class VIPInitCheck extends BaseCheck {
+	public function check( $files ) {
+		$path 	= $this->get_path(); // So we can ensure we only look at the main functions.php
+		$files 	= $this->filter_files( $files, 'php' );
+
+		$vip_init_found = false;
+
+		foreach( $files as $path => $content ) {
+			if ( ! $this->file_is_main_functions( $path ) )
+				continue;
+
+			$vip_init_found = $this->vip_init_is_included( $path );
+
+			break;
+		}
+
+		if ( ! $vip_init_found )
+			$this->add_error( 'vip-init', 'VIP Init', BaseScanner::LEVEL_BLOCKER, 'functions.php', array( 'vip-init.php was not required' ) );
+
+		return true;
+	}
+
+	public function file_is_main_functions( $file ) {
+		$filename = $this->get_filename( $file );
+
+		if ( 'functions.php' !== $filename )
+			return false;
+
+		// Strip off the main path, so we can tell if $file is in the root - otherwise we'd get false positives for other files
+		// named functions.php in other directories
+		$path = str_replace( $this->get_path(), '', $file );
+
+		if ( '/' !== dirname( $path ) )
+			return false;
+
+		return true;
+	}
+
+	public function vip_init_is_included( $file ) {
+		$matches = $this->preg_file( '/^require[_once]?.*?vip-init\.php/', $file );
+
+		return ( count( $matches ) ) ? true : false;
+	}
+}
