45416ff9343bac88ef80d768cbd369146fee2198
Nick Daugherty <ndaugherty987@gmail.com>
2014-03-01 18:18:09 -0700

    Add PHPShortTagsCheck unit tests

---
+++ tests/checks/test-PHPShortTagsCheck.php
@@ -0,0 +1,71 @@
+<?php
+
+class PHPShortTagsTest extends WP_UnitTestCase {
+	protected $_PHPShortTagsCheck;
+
+	public function setUp() {
+		require_once VIP_SCANNER_DIR . '/checks/PHPShortTagsCheck.php';
+
+		$this->_PHPShortTagsCheck = new PHPShortTagsCheck();
+	}
+
+	public function testValidTags() {
+		$input = array( 
+			'php' => array(
+				'test.php' => '<?php
+
+				echo "doing things";
+
+				?>
+				'
+			)
+		);
+
+		$result = $this->_PHPShortTagsCheck->check( $input );
+
+		$errors = $this->_PHPShortTagsCheck->get_errors();
+
+		$error_slugs = wp_list_pluck( $errors, 'slug' );
+
+		$this->assertNotContains( 'php-shorttags', $error_slugs );
+	}
+
+	public function testOpeningShortTags() {
+		$input = array( 
+			'php' => array(
+				'test.php' => '<?
+
+				echo "doing things";
+				'
+			)
+		);
+
+		$result = $this->_PHPShortTagsCheck->check( $input );
+
+		$errors = $this->_PHPShortTagsCheck->get_errors();
+
+		$error_slugs = wp_list_pluck( $errors, 'slug' );
+
+		$this->assertContains( 'php-shorttags', $error_slugs );
+	}
+
+	public function testOutputShortTags() {
+		$input = array( 
+			'php' => array(
+				'test.php' => '
+					$foo = "bar";
+
+					<?=$foo; ?>
+				'
+			)
+		);
+
+		$result = $this->_PHPShortTagsCheck->check( $input );
+
+		$errors = $this->_PHPShortTagsCheck->get_errors();
+
+		$error_slugs = wp_list_pluck( $errors, 'slug' );
+
+		$this->assertContains( 'php-shorttags', $error_slugs );
+	}
+}
