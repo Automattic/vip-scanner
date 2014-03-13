9574088b1b7cc3fc974f8d9a35bd910114a7d284
Mobius5150 <mike@michaelblouin.ca>
2014-03-13 12:21:52 -0600

    Adding class to retrieve information about a PHP files' class, functions etc...
    The `AnalyzedFile` class analyzes a file for PHP constructs such as namespaces, classes, functions, etc.. when instantiated. This class will be used for doing theme analysis.
    
    This class uses mostly regular expresssions under the hood to do the parsing. While tokens could've been used instead, using regular expressions allows us to do less parsing of the exact contents of functions and the like at the cost of a bit of obscurity.

---
+++ vip-scanner/class-analyzed-file.php
@@ -0,0 +1,332 @@
+<?php
+
+class AnalyzedFile {
+	protected $filepath = '';
+	protected $filecontents = '';
+	
+	protected $hierarchy_elements = array(
+		'namespaces' => array(),
+		'classes'    => array(),
+		'functions'  => array(),
+	);
+	
+	protected $comments_regex = <<<EOT
+		(\/\*(?:(?!\*\/)[\s\S])*\*\/)			# match a multiline comment
+			|
+		(\/\/(?:(?!\n|\r)[\s\S])*)				# match a single line comment
+EOT
+		;
+	
+	protected $strings_regex = <<<EOT
+		<<<(?<herestart>\S+)((?!\1)[\s\S])*\3;	# match a heredoc
+			|
+		(([\'"])((?!\6)[\s\S])*\6)				# match a string
+EOT
+		;
+	
+	protected $strip_inline_php_regex = '\?>((?!<\?php)[\s\S])*<\?php';
+	
+	protected static $hierarchy_regexes = array(
+		'php'		 => array( 'regex' => <<<EOT
+			(								# start of bracket 1
+				<\?php							# php opening tag
+					(?<contents>((?!\?>)[\s\S])*)   # match anything except a php closing tag
+				(\?>)?							# match a closing tag
+			)								# end of bracket 1
+EOT
+			),
+
+		'namespaces' => array( 'regex' => <<<EOT
+			namespace\s+(?<name>(\\\\?\w+)+);    # Match the name of the namespace
+			(?<contents>((?!namespace)[\s\S])*)	 # match the contents of the namespace
+EOT
+			, ),
+
+		'classes'    => array( 'regex' => <<<EOT
+			((?<abstract>abstract)\s+)? # optionally match an abstract class
+			class\s+(?<name>\w+)\s+ # match the classname
+			(extends\s+(?<parentclass>\w+)\s*)? # optionally match a parentclass 
+			(?<contents>
+				(						# start of bracket 7
+					{                   # match an opening curly bracket
+						(?:
+							[^{}]++     # one or more non curly brackets
+							  |
+							(?7)        # recurse to bracket 7
+						)*
+					}                   # match a closing curly bracket
+				)						# end of bracket 7
+			)
+EOT
+		   , ),
+
+		'functions'  => array( 'regex' => <<<EOT
+			\s*(									# match function modifiers (visibility, static, abstract)
+					\s*(?<visibility>private|protected|public)\s+
+						|
+					\s*(?<static>static)\s+
+						|
+					\s*(?<abstract>abstract)\s+
+				){0,3}
+			\s*function\s+(?<name>[a-zA-Z0-9_]+\s*) # match the function definition & name
+			\((?<args>(\s|\w|[$,_='"])+)?\)\s*		# match the function arguments
+			(?<contents>
+				(?(abstract);|							# match either the semicolon of an abstract function or a closure
+					(									# start of bracket 1
+						{								# match an opening curly bracket
+							(?:
+								[^{}]++					# one or more non curly brackets
+								  |
+								(?8)					# recurse to bracket 1
+							)*
+						}								# match a closing curly bracket
+					)									# end of bracket 1
+				)
+			)
+EOT
+			, ),
+
+		'members'    => array( 'regex' => '', ),
+	);
+	
+	protected static $check_hierarchy = array(
+		'php' => array(
+			'namespaces' => array(
+				'classes' => array(
+					'functions' => array(),
+					'members'   => array(),
+				),
+			),
+
+			'classes' => array(
+				'functions' => array(),
+				'members'	=> array(),
+			),
+
+			'functions' => array(),
+			'members'   => array(),
+		),
+	);
+	
+	public function __construct( $filepath, $filecontents = null ) {
+		$this->filepath = $filepath;
+		$this->filecontents = $filecontents;
+		$this->analyze_file();
+	}
+
+	/**
+	 * Gets the path of the analyzed file.
+	 * @return string
+	 */
+	public function get_filepath() {
+		return $this->filepath;
+	}
+	
+	/**
+	 * Gets the name of the analyzed file.
+	 * @return string
+	 */
+	public function get_filename() {
+		return basename( $this->filepath );
+	}
+	
+	/**
+	 * Gets the stored contents of the analyzed file.
+	 * @return string
+	 */
+	public function get_file_contents() {
+		return $this->filecontents;
+	}
+	
+	/**
+	 * Gets elements of PHP code from a file by their path. This includes things such as namespaces,
+	 * classes, and functions.
+	 * 
+	 * The path is the names of the containers of an element. For example, if a 
+	 * function "myfunc" exists within a class "myclass" its path would be "myclass".
+	 * 
+	 * Specify the $type to get all elements of that type. If $path is null you 
+	 * will receive all functions in the file, including namespaced functions and
+	 * class members.
+	 * 
+	 * If $path is an empty string it will retrieve only elements of $type within
+	 * the global namespace.
+	 * 
+	 * @param string $type The element type. "namespaces", "classes", or "functions"
+	 * @param string $path [optional] The path to the elements. Empty string for global.
+	 * @return array
+	 */
+	public function get_code_elements( $type, $path = null ) {
+		if ( ! array_key_exists( $type, $this->hierarchy_elements ) ) {
+			return array();
+		}
+		
+		if ( is_null( $path ) ) {
+			return $this->hierarchy_elements[$type];
+		} else {
+			if ( array_key_exists( $path, $this->hierarchy_elements[$type] ) ) {
+				return $this->hierarchy_elements[$type][$path];
+			} else {
+				return array();
+			}
+		}
+	}
+	
+	/**
+	 * Retrieves the check hierarchy
+	 * @return array
+	 */
+	public function get_check_hierarchy() {
+		return self::$check_hierarchy;
+	}
+
+	/**
+	 * Analyzes this file.
+	 */
+	protected function analyze_file() {
+		// Load the contents of the file
+		if ( is_null( $this->filecontents ) || ! $this->filecontents ) {
+			$this->filecontents = file_get_contents( $this->filepath );
+		}
+		
+		if ( false === $this->filecontents ) {
+			return;
+		}
+		
+		// Strip strings and comments from the file. Preserve line numbers
+		$stripped = $this->strip_strings_and_comments( $this->filecontents );
+		
+		// Do the php check hierarchy
+		$this->do_check_hierarchy( '', self::$check_hierarchy, $stripped, 0 );
+	}
+	
+	/**
+	 * Runs through a check hierarchy recursively. Assumes that the incoming
+	 * contents have already been stripped of strings and comments.
+	 * 
+	 * Typically uses the hierarchy AnalyzedFile::$check_hierarchy
+	 * 
+	 * The general algorithm is this:
+	 * 1. Iterate over the first level of the passed in hierarchy.
+	 *	1. Apply the regex for this level. Every regex (if there's a match)
+	 *		should have a named capture group "contents". This would be the contents
+	 *		of a code block for example.
+	 *	2. Record all of the names of the matches. For example if we match a "class"
+	 *		we record all of the class names.
+	 *	3. Recurse to the next level of the hierarchy, passing on the contents from 1.1. 
+	 *		If the last level was "class" then this may be functions.
+	 * 2. Remove everything that we've analyzed from the input string. This stops
+	 *		items within code block (such as functions in classes) from being matched
+	 *		twice. This step preserves line numbers.
+	 * 
+	 * @param string $top_level The name of the container of this level of hierarchy.
+	 * @param array $hierarchy The hierarchy of checks to run.
+	 * @param string $contents The contents to run the checks on
+	 * @param number $line_offset The offset to be added to line numbers recorded by matches
+	 */
+	protected function do_check_hierarchy( $top_level, $hierarchy, $contents, $line_offset ) {
+		foreach ( $hierarchy as $level => $children ) {
+			// Get the regex for this element
+			$regex = self::$hierarchy_regexes[$level]['regex'];
+
+			if ( empty( $regex ) ) continue;
+
+			// Get all of the matches
+			preg_match_all( "/$regex/ix", $contents, $matches, PREG_OFFSET_CAPTURE );
+
+			// Go over each match
+			foreach ( $matches['contents'] as $index => $match ) {
+				$this_top_level = '';
+				$this_line_number = $this->compute_line_number( $contents, $match[1], $line_offset );
+
+				// Save this match (only saved named capture groups)
+				if ( isset( $this->hierarchy_elements[$level] ) ) {
+
+					if ( empty( $top_level ) ) {
+						$this_top_level = $matches['name'][$index][0];
+					} else {
+						$this_top_level = "$top_level::{$matches['name'][$index][0]}";
+					}
+
+					$this_match = array( 'line' => $this_line_number );
+
+					// Save this matches properties
+					foreach ( $matches as $match_type => $individual_matches ) {
+						if ( ! is_numeric( $match_type ) ) {
+							$this_match[$match_type] = $individual_matches[$index][0];
+						}
+					}
+					
+					if ( ! array_key_exists( $top_level, $this->hierarchy_elements[$level] ) ) {
+						$this->hierarchy_elements[$level][$top_level] = array();
+					}
+
+					$this->hierarchy_elements[$level][$top_level][$this_top_level] = $this_match;
+				}
+
+				// Parse any children in the hierarchy. Subtract 1 from line number because
+				// The first line of the next hierarchy is on the current line
+				$this->do_check_hierarchy( $this_top_level, $children, $match[0], $this_line_number - 1 );
+			}
+
+			// Replace substring (to prevent double matches) and calculate line offsets
+			foreach ( $matches[0] as $index => $match ) {
+				$this->replace_match_with_whitespace( $contents, $matches[0][$index][0] );
+			}
+		}
+	}
+	
+	/**
+	 * Counts the line number of the char at index $char in the $string.
+	 * 
+	 * @param string $string
+	 * @param number $char
+	 * @param number $line_offset
+	 */
+	protected function compute_line_number( $string, $char, $line_offset = 0 ) {
+		preg_match_all( "/\r\n|\r|\n/", substr( $string, 0, $char ), $matches );
+		
+		return count( $matches[0] ) + 1 + $line_offset;
+	}
+
+	/**
+	 * Replaces $match in $contents with whitespace, preserving line numbers.
+	 * Modifies the $contents variable.
+	 * 
+	 * @param string $contents
+	 * @param string $match
+	 */
+	protected function replace_match_with_whitespace( &$contents, $match ) {
+		// Count the number of lines we're about to remove
+		preg_match_all( "/\r\n|\r|\n/", $match, $line_end_matches );
+		$mod_line_count = count( $line_end_matches[0] );
+
+		// Remove the contents of these lines while maintaining the overall line count
+		$pos = strpos( $contents, $match );
+		$contents = substr_replace( $contents, str_repeat( "\n", $mod_line_count ) , $pos, strlen( $match ) );
+	}
+	
+	/**
+	 * Strips all strings and comments from $contents. Attempts to preserve line numbers.
+	 * 
+	 * @param string $contents The initial contents
+	 * @return string $contents with strings and comments stripped out
+	 */
+	protected function strip_strings_and_comments( $contents ) {
+		$regexes = array(
+			$this->comments_regex,
+			$this->strings_regex,
+			$this->strip_inline_php_regex,
+		);
+		
+		foreach ( $regexes as $regex ) {
+			preg_match_all( "/{$regex}/ix", $contents, $matches );
+
+			foreach ( $matches[0] as $match ) {
+				$this->replace_match_with_whitespace( $contents, $match );
+			}
+		}
+
+		return $contents;
+	}
+}
