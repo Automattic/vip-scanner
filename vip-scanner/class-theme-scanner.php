33cbf6e5276027ff9156f53205842c620ee790a6
Matt Perry <mattoperry@gmail.com>
2014-04-16 11:20:16 -0700

    cli theme scans now support full paths
    - you can specify a fully-qualified path for a theme in your theme scan
    on wp-cli
    - still supports naming the theme too (i.e.: vip/*theme* or pub/*theme*

--- vip-scanner/class-theme-scanner.php
+++ vip-scanner/class-theme-scanner.php
@@ -6,9 +6,14 @@ class ThemeScanner extends DirectoryScanner
 		if( ! function_exists( 'get_theme_root' ) )
 			return $this->add_error( 'wp-load', sprintf( '%s requires WordPress to be loaded.', get_class( $this ) ), 'blocker' );
 
-		// Get Theme Path
-		$path = sprintf( '%s/%s', get_theme_root(), $theme );
+        //decide whether to interpret theme as a path
 
+        if ( ( strpos( $theme, '/') !== false ) && !in_array( substr( $theme, 0, 4), array( 'pub/', 'vip/' ) ) ) {
+            $path = $theme;
+        }else{
+            $path = sprintf( '%s/%s', get_theme_root(), $theme );
+        }
+        
 		// Call Parent Constructor
 		parent::__construct( $path, $review );
 	}
