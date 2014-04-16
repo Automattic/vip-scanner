d811fb222b5ba1790599dc65795ee23d9e6721ab
Matt Perry <mattoperry@gmail.com>
2014-04-16 11:24:43 -0700

    replaces spaces with tabs
    - editor configuration problem caused use of spaces rather than tab
    characters in previous commit.  fixing.

--- vip-scanner/class-theme-scanner.php
+++ vip-scanner/class-theme-scanner.php
@@ -6,14 +6,13 @@ class ThemeScanner extends DirectoryScanner
 		if( ! function_exists( 'get_theme_root' ) )
 			return $this->add_error( 'wp-load', sprintf( '%s requires WordPress to be loaded.', get_class( $this ) ), 'blocker' );
 
-        //decide whether to interpret theme as a path
+ 		//decide whether to interpret theme as a path
+		if ( ( strpos( $theme, '/') !== false ) && !in_array( substr( $theme, 0, 4), array( 'pub/', 'vip/' ) ) ) {
+			$path = $theme;
+		}else{
+			$path = sprintf( '%s/%s', get_theme_root(), $theme );
+		}
 
-        if ( ( strpos( $theme, '/') !== false ) && !in_array( substr( $theme, 0, 4), array( 'pub/', 'vip/' ) ) ) {
-            $path = $theme;
-        }else{
-            $path = sprintf( '%s/%s', get_theme_root(), $theme );
-        }
-        
 		// Call Parent Constructor
 		parent::__construct( $path, $review );
 	}
