0061ddbce1749b972696c9724d170c3df1fb50c2
Fränk Klein <contact@fklein.info>
2014-06-25 22:18:27 +0200

    Add check for forbidden functions in WP.com themes.

---
+++ vip-scanner/checks/ForbiddenFunctionsCheck.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Checks for usage of functions that themes should not use:
+ *
+ * register_post_type()
+ * register_taxonomy()
+ * add_shortcode()
+ * add_meta_box()
+ * add_help_tab() (WP_Screen method)
+ * query_posts()
+ */
+
+class ForbiddenFunctionsCheck extends BaseCheck {
+
+	function check( $files ) {
+
+		$forbidden_functions = array(
+			'register_post_type',
+			'register_taxonomy',
+			'add_shortcode',
+			'add_meta_box',
+			'add_help_tab',
+			'query_posts',
+		);
+
+		$php = $this->merge_files( $files, 'php' );
+
+
+		foreach ( $forbidden_functions as $function ) {
+			$this->increment_check_count();
+
+			if ( false !== strpos( $php, $function ) ) {
+				$this->add_error(
+					'forbidden-function',
+					sprintf( 'The function %s was found in the theme. Themes cannot use this function, please remove it.', '<code>' . $function . '</code>' ),
+					'blocker'
+				);
+				$result = false;
+			}
+		}
+		return $result;
+	}
+}
--- vip-scanner/config-vip-scanner.php
+++ vip-scanner/config-vip-scanner.php
@@ -51,6 +51,7 @@ VIP_Scanner::get_instance()->register_review( 'Undefined Function Check', array(
 
 VIP_Scanner::get_instance()->register_review( 'WP.com Theme Review', array(
 	'EscapingCheck',
+	'ForbiddenFunctionsCheck',
 	'InternationalizedStringCheck',
 	'TheamPubFileCheck',
 	'TheamPubIndividualFiles',
