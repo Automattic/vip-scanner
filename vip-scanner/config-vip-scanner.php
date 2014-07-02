8182921afdf48cf59becab4f747967df288e96b3
Fränk Klein <contact@fklein.info>
2014-07-02 12:19:02 +0200

    Remove outdated and unused WP.org Review Type.

--- vip-scanner/config-vip-scanner.php
+++ vip-scanner/config-vip-scanner.php
@@ -5,46 +5,6 @@ include_once( VIP_SCANNER_DIR . '/config-vip-scanner-wpcom-form.php' );
 
 // TODO: Not sure if this is the best place or way to handle this...
 
-VIP_Scanner::get_instance()->register_review( 'WP.org Theme Review', array(
-	'ThemeArtisteerCheck',
-	'BadThingsCheck',
-	'ThemeBasicChecks',
-	'ThemeCommentPaginationCheck',
-	'ThemeContentWidthCheck',
-	'ThemeStyleOptionalCheck',
-	'ThemeCustomizeCheck',
-	'DeprecatedCheck',
-	'DirectoriesCheck',
-	'ThemeEditorStyleCheck',
-	'ThemeFilesCheck',
-	'GravatarCheck',
-	'ThemeIncludeCheck',
-	'HardcodedLinksCheck',
-	'MalwareCheck',
-	'MoreDeprecatedCheck',
-	'ThemeNavMenuCheck',
-	'NonPrintableCheck',
-	'PHPShortTagsCheck',
-	'PHPClosingTagsCheck',
-	'ThemePostFormatCheck',
-	'ThemePostPaginationCheck',
-	'ThemePostThumbnailCheck',
-	'GetOptionDeprecated',
-	'ThemeStyleRequiredCheck',
-	'ThemeStyleSuggestedCheck',
-	'BloginfoDeprecatedCheck',
-	'ThemeTagCheck',
-	'ThemeSupportCheck',
-	'TimeDateCheck',
-	'TimThumbCheck',
-	'WormCheck',
-	'VCMergeConflictCheck',
-), array(
-	'PHPAnalyzer',
-	'CustomResourceAnalyzer',
-	'ThemeAnalyzer',
-) );
-
 VIP_Scanner::get_instance()->register_review( 'Undefined Function Check', array(
 	'UndefinedFunctionCheck',
 ) );
