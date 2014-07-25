9f842fa012d0c9884484de608985b2b7dc7d8c7c
Fränk Klein <contact@fklein.info>
2014-07-25 13:30:46 +0200

    Add AtPackage check.
    Verifies that every PHP file in a theme has a correct @package header.

---
+++ vip-scanner/checks/AtPackageCheck.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Checks for correct @package information.
+ */
+
+class AtPackageCheck extends BaseCheck {
+
+	function check( $files ) {
+
+		$result = true;
+		$php_files = $this->filter_files( $files, 'php' );
+
+		/**
+		 * Check for wrong @package WordPress.
+		 */
+		$this->increment_check_count();
+
+		foreach ( $php_files as $file_path => $file_content ) {
+			if ( false !== stripos( $file_content, '@package WordPress' ) ) {
+				$this->add_error(
+					'atpackage',
+					'The theme uses <code>@package WordPress</code>. This is reserved for WordPress Core files.',
+					Basescanner::LEVEL_WARNING,
+					$this->get_filename( $file_path )
+				);
+				$result = false;
+			}
+		}
+
+		/**
+		 * Check if every PHP file contains @package information. 
+		 */
+		$this->increment_check_count();
+
+		foreach ( $php_files as $file_path => $file_content ) {
+			if ( false === stripos( $file_content, '@package' ) ) {
+				$this->add_error(
+					'atpackage',
+					'The file does not contain <code>@package</code> information. Every PHP file should have <code>@package Themename</code> in its header.',
+					Basescanner::LEVEL_WARNING,
+					$this->get_filename( $file_path )
+				);
+				$result = false;
+			}
+		}
+
+		return $result;
+	}
+}
--- vip-scanner/config-vip-scanner.php
+++ vip-scanner/config-vip-scanner.php
@@ -10,6 +10,7 @@ VIP_Scanner::get_instance()->register_review( 'Undefined Function Check', array(
 ) );
 
 VIP_Scanner::get_instance()->register_review( 'WP.com Theme Review', array(
+	'AtPackageCheck',
 	'BodyClassCheck',
 	'CommentsCheck',
 	'CustomizerCheck',
