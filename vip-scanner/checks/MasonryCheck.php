6c83327e3844cbd016dce9a7dffe39922916d338
Fränk Klein <contact@fklein.info>
2014-07-28 22:06:58 +0200

    Check for deprecated Masonry.
    Looks for options and methods deprecated with Masonry V3.

--- vip-scanner/checks/MasonryCheck.php
+++ vip-scanner/checks/MasonryCheck.php
@@ -2,6 +2,7 @@
 /**
  * Checks for Masonry v3, that comes bundled with WordPress 3.9.
  * Check if the theme uses the old 'jquery-masonry' script handle.
+ * Check for deprecated options and methods.
  */
 
 class MasonryCheck extends BaseCheck {
@@ -27,6 +28,39 @@ class MasonryCheck extends BaseCheck {
 				$result = false;
 			}
 		}
+
+		/**
+		 * Check for deprecated options and methods: isRTL option, isResizable option, gutterWidth option, and layout method.
+		 */
+		$this->increment_check_count();
+
+		$js_files = $this->filter_files( $files, 'js' );
+
+		$deprecated = array(
+			'isRTL'        => 'The <code>isRTL</code> option has been removed, use <code>isOriginLeft: false</code> instead.',
+			'isResizable:' => 'The <code>isResizable</code> option has been renamed to <code>isResizeBound</code>.',
+			'gutterWidth'  => 'The <code>gutterWidth</code> option has been renamed to <code>gutter</code>.',
+			'layout:'      => 'The <code>layout</code> method has been renamed to <code>layoutItems</code>.',
+		);
+
+		foreach( $js_files as $file_path => $file_content ) {
+			// Look if the file concerns Masonry.
+			if ( false !== stripos( $file_content, 'Masonry' ) ) {
+				// Look for the deprecated options.
+				foreach ( $deprecated as $code => $message ) {
+					if ( false !== strpos( $file_content, $code . ':' ) ) {
+						$this->add_error(
+							'masonry',
+							'Found deprecated jQuery option. ' . $message,
+							Basescanner::LEVEL_BLOCKER,
+							$this->get_filename( $file_path )
+						);
+						$result = false;
+					}
+				}
+			}
+		}
+
 
 		return $result;
 	}
