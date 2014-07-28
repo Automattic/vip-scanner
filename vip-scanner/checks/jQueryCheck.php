b101d9503b1cab2d0171f63f5d0af22cca30ab77
Fränk Klein <contact@fklein.info>
2014-07-28 14:16:44 +0200

    Add check for deprecated $(document).on( "ready"
    $(document).on( "ready", handler ) is deprecated as of jQuery 1.8.

--- vip-scanner/checks/jQueryCheck.php
+++ vip-scanner/checks/jQueryCheck.php
@@ -2,6 +2,7 @@
 /**
  * Checks for jQuery:
  * Check for deprecated .live() method.
+ * Check for deprecated $(document).on( "ready", handler ).
  */
 
 class jQueryCheck extends BaseCheck {
@@ -21,6 +22,22 @@ class jQueryCheck extends BaseCheck {
 				$this->add_error(
 					'jquery',
 					'The jQuery <code>.live()</code> method is deprecated. Use <code>.on()</code> instead to attach event handlers.',
+					Basescanner::LEVEL_BLOCKER,
+					$this->get_filename( $file_path )
+				);
+				$result = false;
+			}
+		}
+
+		/**
+		 * Check for deprecated $(document).on( "ready", handler )
+		 */
+		$this->increment_check_count();
+		foreach( $js_files as $file_path => $file_content ) {
+			if ( ! preg_match( '/\$\(\s*document\s*\)\.on\(\s*[\'"]ready[\'"]\s*,\s*function\(\)/', $file_content ) ) {
+				$this->add_error(
+					'jquery',
+					'The jQuery <code>$(document).on( "ready", handler )</code> handler is deprecated deprecated as of jQuery 1.8.',
 					Basescanner::LEVEL_BLOCKER,
 					$this->get_filename( $file_path )
 				);
