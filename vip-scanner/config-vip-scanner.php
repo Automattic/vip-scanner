0e874e100ee191d2157441a15dbf72b52212f4ea
Fränk Klein <contact@fklein.info>
2014-08-05 19:39:56 +0200

    Add CDNCheck.
    Checks whether JS or CSS resources are loaded from a CDN. Looks for the
    CDN URL of popular libraries and frameworks.

--- vip-scanner/config-vip-scanner.php
+++ vip-scanner/config-vip-scanner.php
@@ -12,6 +12,7 @@ VIP_Scanner::get_instance()->register_review( 'Undefined Function Check', array(
 VIP_Scanner::get_instance()->register_review( 'WP.com Theme Review', array(
 	'AtPackageCheck',
 	'BodyClassCheck',
+	'CDNCheck',
 	'CommentsCheck',
 	'CustomizerCheck',
 	'DeprecatedConstantsCheck',
