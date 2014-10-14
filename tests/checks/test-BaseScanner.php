267d22a685d7607867f199c0a94e913da6007ef1
david.binda <david.binovec@gmail.com>
2014-10-14 15:21:48 +0200

    Adds a test method for testing a propper functionality of get_file_type method of BaseScanner As we are using get_file_type method in one of our tests, we should make sure that the method is WaD first. The method is testing some basic file types + an swf file type as that one is the only used in other tests currently

--- tests/checks/test-BaseScanner.php
+++ tests/checks/test-BaseScanner.php
@@ -19,6 +19,17 @@ class BaseScannerTest extends WP_UnitTestCase {
 		$this->_BaseScanner = new BaseScanner( $files, array() );
 	}
 
+	public function test_get_file_type() {
+		$filenames = array (
+			'swf' => '/test-theme/file.swf',
+			'php' => '/test-theme/files/index.php',
+			'html' => '/test-theme/file.html',
+		);
+		foreach( $filenames as $type => $filename ) {
+			$this->assertEquals( $type, $this->_BaseScanner->get_file_type( $filename ) );
+		}
+	}
+
 	public function test_check_filename_for_swf_file() {
 		$filename = '/file.swf';
 		$this->assertFalse( $this->_BaseScanner->check_filename( $filename, $this->_BaseScanner->get_file_type( $filename ) ) );
