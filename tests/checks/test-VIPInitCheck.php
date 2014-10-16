02119f8f61aafe54486b26ea40c37f20b0490a14
david.binda <david.binovec@gmail.com>
2014-10-16 16:00:39 +0200

    Adds a test for vip_init.php file inclusion As it was reported, there might be some false positive errors about checking a requirement of vip_init.php from functions.php. This test should make sure that this is no longer true

---
+++ tests/checks/test-VIPInitCheck.php
@@ -0,0 +1,23 @@
+<?php
+
+class VIPInitCheckTest extends WP_UnitTestCase {
+	protected $_VIPInitCheckTest;
+	protected $_functions_file;
+
+	public function setUp() {
+		parent::setUp();
+		require_once VIP_SCANNER_DIR . '/checks/VIPInitCheck.php';
+
+		$this->_VIPInitCheckTest = new VIPInitCheck();
+		$this->_functions_file = dirname(__FILE__) . '/../data/functions.php';
+	}
+
+	public function testIsMainFunctions() {
+		$this->assertTrue( $this->_VIPInitCheckTest->file_is_main_functions( $this->_functions_file ) );
+	}
+
+	public function testCheckRequire() {
+		$this->assertTrue( $this->_VIPInitCheckTest->vip_init_is_included( $this->_functions_file ) );
+	}
+
+}
---
+++ tests/data/functions.php
@@ -0,0 +1,4 @@
+<?php
+
+// Init WP.com VIP environment
+require_once( WP_CONTENT_DIR . '/themes/vip/plugins/vip-init.php' );
