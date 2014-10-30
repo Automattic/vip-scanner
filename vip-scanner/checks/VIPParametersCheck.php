cb6a1a13b32c5830e1e78797ea814411c51daf52
Yannick Lefebvre <ylefebvre@gmail.com>
2014-10-29 21:46:33 -0400

    Moved check logic to new method to be able to write more thorough unit tests
    Many of the checks introduced in the VIP parameters check could not be exercised since the only checks in place so far were for the first parameter of a single parameter function. By moving the checking logic to a separate method that received the file path, file contents and list of checks as arguments, more thorough unit tests can be written.

--- vip-scanner/checks/VIPParametersCheck.php
+++ vip-scanner/checks/VIPParametersCheck.php
@@ -60,30 +60,39 @@ class VIPParametersCheck extends BaseCheck {
 		);
 
 		foreach ( $this->filter_files( $files, 'php' ) as $file_path => $file_content ) {
+			if ( false == $this->check_file_contents( $checks, $file_path, $file_content ) ) {
+				$result = false;
+			}
+		}
 
-			// Loop through all functions.
-			foreach ( $checks as $function => $data ) {
+		return $result;
+	}
 
-				// Loop through the parameters and look for all function/parameter combinations.
-				foreach ( $data as $parameter_data ) {
-					$previous_params = '';
-					if ( isset( $parameter_data['position'] ) && $parameter_data['position'] > 0 ) {
-						$previous_params = '(("|\')?(.+)("|\')?,\s*){' . $parameter_data['position'] . '}';
-					} elseif ( ! isset( $parameter_data['position'] ) ) {
-						$previous_params = '(.+)';
-					}
+	function check_file_contents( $checks, $file_path, $file_content ) {
+		$result = true;
 
-					if ( preg_match( '/' . $function . '\(\s*' . $previous_params . '("|\'|\s)' . $parameter_data['value'] . '("|\'|,|\s)\s*/', $file_content, $matches ) ) {
-						$lines = $this->grep_content( $matches[0], $file_content );
-						$this->add_error(
-							$parameter_data['value'],
-							esc_html( $parameter_data['note'] ),
-							$parameter_data['level'],
-							$this->get_filename( $file_path ),
-							$lines
-						);
-						$result = false;
-					}
+		// Loop through all functions.
+		foreach ( $checks as $function => $data ) {
+
+			// Loop through the parameters and look for all function/parameter combinations.
+			foreach ( $data as $parameter_data ) {
+				$previous_params = '';
+				if ( isset( $parameter_data['position'] ) && $parameter_data['position'] > 0 ) {
+					$previous_params = '(("|\')?(.+)("|\')?,\s*){' . $parameter_data['position'] . '}';
+				} elseif ( ! isset( $parameter_data['position'] ) ) {
+					$previous_params = '(.+)';
+				}
+
+				if ( preg_match( '/' . $function . '\(\s*' . $previous_params . '("|\'|\s)' . $parameter_data['value'] . '("|\'|,|\s)\s*/', $file_content, $matches ) ) {
+					$lines = $this->grep_content( $matches[0], $file_content );
+					$this->add_error(
+						$parameter_data['value'],
+						esc_html( $parameter_data['note'] ),
+						$parameter_data['level'],
+						$this->get_filename( $file_path ),
+						$lines
+					);
+					$result = false;
 				}
 			}
 		}
