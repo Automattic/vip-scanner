adf21d5dd192b6a56b53d50b4e80f61538c9fc3d
Yannick Lefebvre <ylefebvre@gmail.com>
2014-11-05 07:57:16 -0500

    Updated add_error slug to generic component and parameter value
    This makes slugs unique and describes where they came from.

--- vip-scanner/checks/VIPParametersCheck.php
+++ vip-scanner/checks/VIPParametersCheck.php
@@ -86,7 +86,7 @@ class VIPParametersCheck extends BaseCheck {
 				if ( preg_match( '/' . $function . '\(\s*' . $previous_params . '("|\'|\s)' . $parameter_data['value'] . '("|\'|,|\s)\s*/', $file_content, $matches ) ) {
 					$lines = $this->grep_content( $matches[0], $file_content );
 					$this->add_error(
-						'vip-parameters',
+						'vip-parameters-' . $parameter_data['value'],
 						esc_html( $parameter_data['note'] ),
 						$parameter_data['level'],
 						$this->get_filename( $file_path ),
