aa9362cd05f9f4aec22afbea9001b218ffc8b386
Yannick Lefebvre <ylefebvre@gmail.com>
2014-11-04 20:07:18 -0500

    Added unit tests for Byte Order Mark
    Added test data file with UTF-8 encoding, along with test functions to test file with BOM and plain file contents that does not have a BOM.

--- tests/checks/test-VIPRestrictedPatternsCheck.php
+++ tests/checks/test-VIPRestrictedPatternsCheck.php
@@ -2,6 +2,11 @@
 
 class VIPRestrictedPatternsTest extends WP_UnitTestCase {
 	protected $_VIPRestrictedPatternsCheck;
+	protected static $BOM_test_file;
+
+	public static function setUpBeforeClass() {
+		self::$BOM_test_file = file_get_contents( dirname( __FILE__ ) . '/../data/test-bom.php' );
+	}
 
 	public function setUp() {
 		parent::setUp();
@@ -102,4 +107,44 @@ class VIPRestrictedPatternsTest extends WP_UnitTestCase {
 		$this->assertTrue( $result );
 	}
 
+	public function testByteOrderMark() {
+		$input = array(
+			'php' => array(
+				'test.php' => self::$BOM_test_file
+			)
+		);
+
+		$result = $this->_VIPRestrictedPatternsCheck->check( $input );
+
+		$errors = $this->_VIPRestrictedPatternsCheck->get_errors();
+
+		$error_slugs = wp_list_pluck( $errors, 'slug' );
+
+		$this->assertContains( '/(\xFE|\xFF|\xFE\xFF|\xFF\xFE|\xEF\xBB\xBF|\x2B\x2F\x76|\xF7\x64\x4C|\x0E\xFE\xFF|\xFB\xEE\x28|\x00\x00\xFE\xFF|\xDD\x73\x66\x73|\x84\x31\x95\x33)/', $error_slugs );
+
+		$this->assertFalse( $result );
+	}
+
+	public function testNoByteOrderMark() {
+		$input = array(
+			'php' => array(
+				'test.php' => '<?php
+
+				if( $imnormal == "hey" )
+					echo "how are you?";
+
+				?>
+				'
+			)
+		);
+
+		$result = $this->_VIPRestrictedPatternsCheck->check( $input );
+
+		$errors = $this->_VIPRestrictedPatternsCheck->get_errors();
+
+		$error_slugs = wp_list_pluck( $errors, 'slug' );
+
+		$this->assertNotContains( '/(\xFE|\xFF|\xFE\xFF|\xFF\xFE|\xEF\xBB\xBF|\x2B\x2F\x76|\xF7\x64\x4C|\x0E\xFE\xFF|\xFB\xEE\x28|\x00\x00\xFE\xFF|\xDD\x73\x66\x73|\x84\x31\x95\x33)/', $error_slugs );
+		$this->assertTrue( $result );
+	}
 }
---
+++ tests/data/test-bom.php
@@ -0,0 +1,5 @@
+﻿<?php
+
+function BOMTest() {
+	// Dummy function
+}
