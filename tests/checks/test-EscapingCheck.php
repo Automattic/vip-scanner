66f433706a39b5b44cf6059aec03d705a9135ea0
Steve Grunwell <steve.grunwell@10up.com>
2015-02-26 17:38:19 +0000

    Split assertions for the_* inside esc_* into separate tests

--- tests/checks/test-EscapingCheck.php
+++ tests/checks/test-EscapingCheck.php
@@ -10,7 +10,6 @@ class EscapingTest extends CheckTestBase {
 				esc_attr( printf( 'Test # %d', $n ) );
 			?>
 EOT;
-
 		$error_slugs = $this->runCheck( $file_contents );
 
 		$this->assertContains( 'functions-file', $error_slugs );
@@ -22,7 +21,6 @@ EOT;
 				esc_attr( print 'Test' );
 			?>
 EOT;
-
 		$error_slugs = $this->runCheck( $file_contents );
 
 		$this->assertContains( 'functions-file', $error_slugs );
@@ -34,7 +32,6 @@ EOT;
 				esc_attr( echo 'Test' );
 			?>
 EOT;
-
 		$error_slugs = $this->runCheck( $file_contents );
 
 		$this->assertContains( 'functions-file', $error_slugs );
@@ -44,7 +41,6 @@ EOT;
 		$file_contents = <<<'EOT'
 			<a href="#" title="<?php _e( 'Doing it wrong' ); ?>">
 EOT;
-
 		$error_slugs = $this->runCheck( $file_contents );
 
 		$this->assertContains( 'functions-file', $error_slugs );
@@ -55,26 +51,63 @@ EOT;
 		$file_contents = <<<'EOT'
 			<a href="#" title='<?php _e( 'Doing it wrong' ); ?>''>
 EOT;
-
 		$error_slugs = $this->runCheck( $file_contents );
 
 		$this->assertContains( 'functions-file', $error_slugs );
 	}
 
-	public function test_printing_template_tags_in_escape_functions() {
+	public function test_the_permalink_in_esc_url() {
 		$file_contents = <<<'EOT'
 			<a href="<?php echo esc_url( the_permalink() ); ?>">
+EOT;
+		$error_slugs = $this->runCheck( $file_contents );
+
+		$this->assertContains( 'functions-file', $error_slugs );
+	}
+
+	public function test_the_permalink_in_esc_url_raw() {
+		$file_contents = <<<'EOT'
 			<a href="<?php print esc_url_raw( the_permalink() ); ?>">
+EOT;
+		$error_slugs = $this->runCheck( $file_contents );
+
+		$this->assertContains( 'functions-file', $error_slugs );
+	}
+
+	public function test_the_title_in_esc_html() {
+		$file_contents = <<<'EOT'
 			<h2><?php echo esc_html( the_title() ); ?></h2>
+EOT;
+		$error_slugs = $this->runCheck( $file_contents );
+
+		$this->assertContains( 'functions-file', $error_slugs );
+	}
+
+	public function test_the_date_in_esc_js() {
+		$file_contents = <<<'EOT'
 			<script><?php echo esc_js( the_date() ) ); ?></script>
+EOT;
+		$error_slugs = $this->runCheck( $file_contents );
+
+		$this->assertContains( 'functions-file', $error_slugs );
+	}
+
+		public function test_the_excerpt_in_esc_textarea() {
+		$file_contents = <<<'EOT'
 			<textarea><?php echo esc_textarea( the_excerpt() ); ?></textarea>
+EOT;
+		$error_slugs = $this->runCheck( $file_contents );
+
+		$this->assertContains( 'functions-file', $error_slugs );
+	}
+
+	public function test_the_content_in_esc_sql() {
+		$file_contents = <<<'EOT'
 			<?php echo esc_sql( the_content() ); ?>
 EOT;
-
 		$error_slugs = $this->runCheck( $file_contents );
-		$expected = array_fill( 0, 6, 'functions-file' ); // One for each instance above
 
-		$this->assertEquals( $expected, $error_slugs );
+		$this->assertContains( 'functions-file', $error_slugs );
 	}
 
 }
