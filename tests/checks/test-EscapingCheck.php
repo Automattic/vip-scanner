dcafcaae06285cd9297dfc08e6ab6eaa12e5fb37
Steve Grunwell <steve.grunwell@10up.com>
2015-02-26 00:04:19 +0000

    Detect occurrences of a self-printing (e.g. 'the_*') within an esc_* function, which would cause the output to be un-escaped

--- tests/checks/test-EscapingCheck.php
+++ tests/checks/test-EscapingCheck.php
@@ -61,4 +61,20 @@ EOT;
 		$this->assertContains( 'functions-file', $error_slugs );
 	}
 
+	public function test_printing_template_tags_in_escape_functions() {
+		$file_contents = <<<'EOT'
+			<a href="<?php echo esc_url( the_permalink() ); ?>">
+			<a href="<?php print esc_url_raw( the_permalink() ); ?>">
+			<h2><?php echo esc_html( the_title() ); ?></h2>
+			<script><?php echo esc_js( the_date() ) ); ?></script>
+			<textarea><?php echo esc_textarea( the_excerpt() ); ?></textarea>
+			<?php echo esc_sql( the_content() ); ?>
+EOT;
+
+		$error_slugs = $this->runCheck( $file_contents );
+		$expected = array_fill( 0, 6, 'functions-file' ); // One for each instance above
+
+		$this->assertEquals( $expected, $error_slugs );
+	}
+
 }
