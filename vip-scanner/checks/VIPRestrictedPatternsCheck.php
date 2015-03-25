94b312d440ba2bf92fbf700fd754a7237a535e82
Derrick Tennant <derrick@decarbonated.org>
2015-03-25 01:01:01 -0400

    VIP Scanner: Updated VIPRestrictedPatternsCheck.php to follow the current WordPress PHP Coding Standards.
    Associative array values are now on a newline with the last array item having a trailing comma and double quotes have been replaced with single quotes on strings where evaluation is not needed.
    The change to the arrays will help when adding any new items to the $check sub-arrays

--- vip-scanner/checks/VIPRestrictedPatternsCheck.php
+++ vip-scanner/checks/VIPRestrictedPatternsCheck.php
@@ -6,21 +6,66 @@ class VIPRestrictedPatternsCheck extends BaseCheck
 		$result = true;
 
 		$checks = array(
-			"/(\\\$isIE)+/msiU" => array( "level" => "Warning", "note" => 'Using $isIE conflicts with full page caching' ),
-			"/(\\\$_REQUEST)+/msiU" => array( "level" => "Blocker", "note" => 'Using $_REQUEST is forbidden. You should use $_POST or $_GET' ),
-			"/WordPress VIP/msiU" => array( "level" => "Warning", "note" => 'Please use "WordPress.com VIP" rather than "WordPress VIP"' ),
-			"/(\\\$wpdb->|mysql_)+.+(ALTER)+\s+/msiU" => array( "level" => "Blocker", "note" => "Possible database table alteration" ),
-			"/(\\\$wpdb->|mysql_)+.+(CREATE)+\s+/msiU" => array( "level" => "Blocker", "note" => "Possible database table creation" ),
-			"/(\\\$wpdb->|mysql_)+.+(DROP)+\s+/msiU" => array( "level" => "Blocker", "note" => "Possible database table deletion" ),
-			"/(\\\$wpdb->|mysql_)+.+(DELETE)+\s+(FROM)+\s+/msiU" => array( "level" => "Warning", "note" => "Direct database delete query" ),
-			"/(\\\$wpdb->|mysql_)+.+(SELECT)+\s.+/msiU" => array( "level" => "Note", "note" => "Direct Database select query" ),
-			"/(^GLOBAL)(\\\$wpdb->|mysql_)+/msiU" => array( "level" => "Warning", "note" => "Possible direct database query" ),
-			'/(echo|\<\?\=)+(?!\s+\(?\s*(?:isset|typeof)\(\s*)[^;]+(\$GLOBALS|\$_SERVER|\$_GET|\$_POST|\$_REQUEST)+/msiU' => array( "level" => "Warning", "note" => "Possible output of restricted variables" ),
-			"/(\\\$GLOBALS|\\\$_SERVER|\\\$_GET|\\\$_POST|\\\$_REQUEST)+/msiU" => array( "level" => "Note", "note" => "Working with superglobals" ),
-			"/(\\\$_SERVER\[(?!('|\"REQUEST_URI|SCRIPT_FILENAME|HTTP_HOST'|\"))([^]]+|)\])+/msiU" => array( "level" => "Blocker", "note" => 'Non whitelisted $_SERVER superglobals found in this file' ),
-			"/(pre_)?option_(blogname|siteurl|post_count)/msiU" => array( "level" => "Blocker", "note" => "possible unsafe use of pre_option_* hook"),
-			"/\\\$wp_query->query_vars\[.*?\][^=]*?\;/msi" => array( "level" => "Warning", "note" => "Possible direct query_vars access, should use get_query_var() function" ),
-			"/\\\$wp_query->query_vars\[.*?\]\s*?\=.*?\;/msi" => array( "level" => "Warning", "note" => "Possible direct query_vars modification, should use set_query_var() function" )
+			'/(\$isIE)+/msiU' => array(
+				'level' => 'Warning',
+				'note'  => 'Using $isIE conflicts with full page caching',
+			),
+			'/(\$_REQUEST)+/msiU' => array(
+				'level' => 'Blocker',
+				'note'  => 'Using $_REQUEST is forbidden. You should use $_POST or $_GET',
+			),
+			'/WordPress VIP/msiU' => array(
+				'level' => 'Warning',
+				'note'  => 'Please use "WordPress.com VIP" rather than "WordPress VIP"',
+			),
+			'/(\$wpdb->|mysql_)+.+(ALTER)+\s+/msiU' => array(
+				'level' => 'Blocker',
+				'note'  => 'Possible database table alteration',
+			),
+			'/(\$wpdb->|mysql_)+.+(CREATE)+\s+/msiU' => array(
+				'level' => 'Blocker',
+				'note'  => 'Possible database table creation',
+			),
+			'/(\$wpdb->|mysql_)+.+(DROP)+\s+/msiU' => array(
+				'level' => 'Blocker',
+				'note'  => "Possible database table deletion"
+			),
+			'/(\$wpdb->|mysql_)+.+(DELETE)+\s+(FROM)+\s+/msiU' => array(
+				'level' => 'Warning',
+				'note'  => "Direct database delete query",
+			),
+			'/(\$wpdb->|mysql_)+.+(SELECT)+\s.+/msiU' => array(
+				'level' => 'Note',
+				'note'  => "Direct Database select query",
+			),
+			'/(^GLOBAL)(\$wpdb->|mysql_)+/msiU' => array(
+				'level' => 'Warning',
+				'note'  => 'Possible direct database query',
+			),
+			'/(echo|\<\?\=)+(?!\s+\(?\s*(?:isset|typeof)\(\s*)[^;]+(\$GLOBALS|\$_SERVER|\$_GET|\$_POST|\$_REQUEST)+/msiU' => array(
+				'level' => 'Warning',
+				'note'  => 'Possible output of restricted variables',
+			),
+			'/(\$GLOBALS|\$_SERVER|\$_GET|\$_POST|\$_REQUEST)+/msiU' => array(
+				'level' => 'Note',
+				'note'  => 'Working with superglobals',
+			),
+			'/(\$_SERVER\[(?!(\'|"REQUEST_URI|SCRIPT_FILENAME|HTTP_HOST\'|"))([^]]+|)\])+/msiU' => array(
+				'level' => 'Blocker',
+				'note'  => 'Non whitelisted $_SERVER superglobals found in this file',
+			),
+			'/(pre_)?option_(blogname|siteurl|post_count)/msiU' => array(
+				'level' => 'Blocker',
+				'note'  => 'possible unsafe use of pre_option_* hook',
+			),
+			'/\$wp_query->query_vars\[.*?\][^=]*?\;/msi' => array(
+				'level' => 'Warning',
+				'note'  => 'Possible direct query_vars access, should use get_query_var() function',
+			),
+			'/\$wp_query->query_vars\[.*?\]\s*?\=.*?\;/msi' => array(
+				'level' => 'Warning',
+				'note'  => 'Possible direct query_vars modification, should use set_query_var() function',
+			),
 		);
 
 		foreach ( $this->filter_files( $files, 'php' ) as $file_path => $file_content ) {
@@ -35,7 +80,7 @@ class VIPRestrictedPatternsCheck extends BaseCheck
 						$check_info['note'],
 						$check_info['level'],
 						$filename,
-						$lines
+						$lines,
 					);
 					$result = false;
 				}
