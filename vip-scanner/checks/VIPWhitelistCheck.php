5c84f848174c1b94c6241b0516da76adaec00e87
Derrick Tennant <derrick@decarbonated.org>
2015-03-25 09:35:32 -0400

    VIP Scanner: Reworked VIPWhitelistCheck's $checks array.
    I feel like swapping the label and regex makes sense because the error label is describing the regex, not the other way around.

--- vip-scanner/checks/VIPWhitelistCheck.php
+++ vip-scanner/checks/VIPWhitelistCheck.php
@@ -8,48 +8,48 @@ class VIPWhitelistCheck extends BaseCheck
 		$php = $this->merge_files( $files, 'php' );
 
 		$checks = array(
-			'/<!DOCTYPE\s+html([^>]{0,})/msiU' => array(
-				'label' => 'no-doctype-defined',
-				'level' => 'Warning',
-				'note'  => 'No doctype defined',
+			'no-doctype-defined' => array(
+				'expression' => '/<!DOCTYPE\s+html([^>]{0,})/msiU',
+				'level'      => 'Warning',
+				'note'       => 'No doctype defined',
 			),
-			'/<html.+(language_attributes){1}([^>]{0,})/msiU' => array(
-				'label' => 'missing-language_attributes',
-				'level' => 'Warning',
-				'note'  => 'No language_attributes() in html tag',
+			'missing-language_attributes' => array(
+				'expression' => '/<html.+(language_attributes){1}([^>]{0,})/msiU',
+				'level'      => 'Warning',
+				'note'       => 'No language_attributes() in html tag',
 			),
-			'/<head.+([^>]{0,})/msiU' => array(
-				'label' => 'missing-profile-attribute',
-				'level' => 'Warning',
-				'note'  => 'Profile attribute missing in head tag',
+			'missing-profile-attribute' => array(
+				'expression' => '/<head.+([^>]{0,})/msiU',
+				'level'      => 'Warning',
+				'note'       => 'Profile attribute missing in head tag',
 			),
 			/*
-			'/<meta\sname="generator"\scontent=".*WordPress.*"([^>]+)/msiU' => array(
-				'label' => 'meta-tag-generator-not-set',
-				'level' => 'Warning',
-				'note'  => 'Meta tag generator not set or not wordpress.com',
+			'meta-tag-generator-not-set' => array(
+				'expression' => '/<meta\sname="generator"\scontent=".*WordPress.*"([^>]+)/msiU',
+				'level'      => 'Warning',
+				'note'       => 'Meta tag generator not set or not wordpress.com',
 			),
 			*/
-			'/(wp_head)+\s?\(\)/msiU' => array(
-				'label' => 'missing-wp_head',
-				'level' => 'Blocker',
-				'note'  => 'wp_head() call missing',
+			'missing-wp_head' => array(
+				'expression' => '/(wp_head)+\s?\(\)/msiU',
+				'level'      => 'Blocker',
+				'note'       => 'wp_head() call missing',
 			),
-			'/(wp_footer)+\s?\(\)/msiU' => array(
-				'label' => 'missing-wp_footer',
-				'level' => 'Blocker',
-				'note'  => 'wp_footer() call missing',
+			'missing-wp_footer' => array(
+				'expression' => '/(wp_footer)+\s?\(\)/msiU',
+				'level'      => 'Blocker',
+				'note'       => 'wp_footer() call missing',
 			),
-			'/(vip_powered_wpcom)+s?\([^\)]*\)/msiU' => array(
-				'label' => 'missing-vip-attribution-link',
-				'level' => 'Blocker',
-				'note'  => 'Attribution link missing, please use <a href="http://vip.wordpress.com/documentation/powered-by-wordpress-com-vip/">vip_powered_wpcom()</a>',
+			'missing-vip-attribution-link' => array(
+				'expression' => '/(vip_powered_wpcom)+s?\([^\)]*\)/msiU',
+				'level'      => 'Blocker',
+				'note'       => 'Attribution link missing, please use <a href="http://vip.wordpress.com/documentation/powered-by-wordpress-com-vip/">vip_powered_wpcom()</a>',
 			),
 		);
 
 		foreach ( $checks as $check => $check_info ) {
 			$this->increment_check_count();
-			if ( ! preg_match( $check, $php ) ) {
+			if ( ! preg_match( $check_info['expression'], $php ) ) {
 				$this->add_error(
 					$check,
 					$check_info['note'],
