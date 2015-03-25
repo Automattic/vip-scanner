87f1a4a0ce4006bd4cab4e511df5bb334736c8d5
Derrick Tennant <derrick@decarbonated.org>
2015-03-25 01:26:17 -0400

    VIP Scanner: Updated VIPWhitelistCheck.php to follow the current WordPress PHP Coding Standards.
    Associative array values are now on a newline with the last array item having a trailing comma and double quotes have been replaced with single quotes on strings where evaluation is not needed.
    The change to the arrays will help when adding any new items to the $check sub-arrays.

--- vip-scanner/checks/VIPWhitelistCheck.php
+++ vip-scanner/checks/VIPWhitelistCheck.php
@@ -8,13 +8,36 @@ class VIPWhitelistCheck extends BaseCheck
 		$php = $this->merge_files( $files, 'php' );
 
 		$checks = array(
-			"/<!DOCTYPE\s+html([^>]{0,})/msiU" => array( "level" => "Warning", "note" => "No doctype defined" ),
-			"/<html.+(language_attributes){1}([^>]{0,})/msiU" => array( "level" => "Warning", "note" => "No language_attributes() in html tag" ),
-			"/<head.+([^>]{0,})/msiU" => array( "level" => "Warning", "note" => "Profile attribute missing in head tag" ),
-			//"/<meta\sname=\"generator\"\scontent=\".*WordPress.*\"([^>]+)/msiU" => array( "level" => "Warning", "note" => "Meta tag generator not set or not wordpress.com" ),
-			"/(wp_head)+\s?\(\)/msiU" => array( "level" => "Blocker", "note" => "wp_head() call missing" ),
-			"/(wp_footer)+\s?\(\)/msiU" => array( "level" => "Blocker", "note" => "wp_footer() call missing" ),
-			"/(vip_powered_wpcom)+s?\([^\)]*\)/msiU" => array( "level" => "Blocker", "note" => "Attribution link missing, please use <a href=\"http://vip.wordpress.com/documentation/powered-by-wordpress-com-vip/\">vip_powered_wpcom()</a>" ),
+			'/<!DOCTYPE\s+html([^>]{0,})/msiU' => array(
+				'level' => 'Warning',
+				'note'  => 'No doctype defined',
+			),
+			'/<html.+(language_attributes){1}([^>]{0,})/msiU' => array(
+				'level' => 'Warning',
+				'note'  => 'No language_attributes() in html tag',
+			),
+			'/<head.+([^>]{0,})/msiU' => array(
+				'level' => 'Warning',
+				'note'  => 'Profile attribute missing in head tag',
+			),
+			/*
+			'/<meta\sname="generator"\scontent=".*WordPress.*"([^>]+)/msiU' => array(
+				'level' => 'Warning',
+				'note'  => 'Meta tag generator not set or not wordpress.com',
+			),
+			*/
+			'/(wp_head)+\s?\(\)/msiU' => array(
+				'level' => 'Blocker',
+				'note'  => 'wp_head() call missing',
+			),
+			'/(wp_footer)+\s?\(\)/msiU' => array(
+				'level' => 'Blocker',
+				'note'  => 'wp_footer() call missing',
+			),
+			'/(vip_powered_wpcom)+s?\([^\)]*\)/msiU' => array(
+				'level' => 'Blocker',
+				'note'  => 'Attribution link missing, please use <a href="http://vip.wordpress.com/documentation/powered-by-wordpress-com-vip/">vip_powered_wpcom()</a>',
+			),
 		);
 
 		foreach ( $checks as $check => $check_info ) {
@@ -23,7 +46,7 @@ class VIPWhitelistCheck extends BaseCheck
 				$this->add_error(
 					$check,
 					$check_info['note'],
-					$check_info['level']
+					$check_info['level'],
 				);
 				$result = false;
 			}
