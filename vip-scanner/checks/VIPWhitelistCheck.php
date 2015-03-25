f8e8afa5f61a49920e03760f4128e2cc3600314f
Derrick Tennant <derrick@decarbonated.org>
2015-03-25 09:08:12 -0400

    Revert "VIP Scanner: Updated VIPWhitelistCheck.php to follow the current WordPress PHP Coding Standards."
    This reverts commit f7328933ba8438702461663b7eee42470d74803c.
    
    Added the entirely wrong commit message.  Let's try again.

--- vip-scanner/checks/VIPWhitelistCheck.php
+++ vip-scanner/checks/VIPWhitelistCheck.php
@@ -9,39 +9,32 @@ class VIPWhitelistCheck extends BaseCheck
 
 		$checks = array(
 			'/<!DOCTYPE\s+html([^>]{0,})/msiU' => array(
-				'label' => 'no-doctype-defined',
 				'level' => 'Warning',
 				'note'  => 'No doctype defined',
 			),
 			'/<html.+(language_attributes){1}([^>]{0,})/msiU' => array(
-				'label' => 'missing-language_attributes',
 				'level' => 'Warning',
 				'note'  => 'No language_attributes() in html tag',
 			),
 			'/<head.+([^>]{0,})/msiU' => array(
-				'label' => 'missing-profile-attribute',
 				'level' => 'Warning',
 				'note'  => 'Profile attribute missing in head tag',
 			),
 			/*
 			'/<meta\sname="generator"\scontent=".*WordPress.*"([^>]+)/msiU' => array(
-				'label' => 'meta-tag-generator-not-set',
 				'level' => 'Warning',
 				'note'  => 'Meta tag generator not set or not wordpress.com',
 			),
 			*/
 			'/(wp_head)+\s?\(\)/msiU' => array(
-				'label' => 'missing-wp_head',
 				'level' => 'Blocker',
 				'note'  => 'wp_head() call missing',
 			),
 			'/(wp_footer)+\s?\(\)/msiU' => array(
-				'label' => 'missing-wp_footer',
 				'level' => 'Blocker',
 				'note'  => 'wp_footer() call missing',
 			),
 			'/(vip_powered_wpcom)+s?\([^\)]*\)/msiU' => array(
-				'label' => 'missing-vip-attribution-link',
 				'level' => 'Blocker',
 				'note'  => 'Attribution link missing, please use <a href="http://vip.wordpress.com/documentation/powered-by-wordpress-com-vip/">vip_powered_wpcom()</a>',
 			),
@@ -53,7 +46,7 @@ class VIPWhitelistCheck extends BaseCheck
 				$this->add_error(
 					$check,
 					$check_info['note'],
-					$check_info['level']
+					$check_info['level'],
 				);
 				$result = false;
 			}
