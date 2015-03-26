ef287015537d51e7cf14e62cf46b97ce097dae7a
Derrick Tennant <derrick@decarbonated.org>
2015-03-26 11:29:35 -0400

    VIP Scanner: Added preliminary support for XSS checks.
    Includes checks for:
    javascript in <link> tag href attributes
    javascript in all HTML tags' style attributes
    -moz-binding CSS attribute

---
+++ tests/checks/test-XSSVectorsCheck.php
@@ -0,0 +1,61 @@
+<?php
+require_once( 'CheckTestBase.php' );
+class XSSVectorsTest extends CheckTestBase {
+
+	public function test_xss_in_link_tag_href() {
+		$file_contents = <<<'EOT'
+			<LINK REL="styleheet" HREF='javascript:alert("XSS");'>
+EOT;
+		$error_slugs = $this->runCheck( $file_contents );
+		$this->assertContains( 'xss-in-link-tag-href', $error_slugs );
+	}
+
+	public function test_obfuscated_xss_in_link_tag_href() {
+		$file_contents = <<<'EOT'
+			<LINK REL="styleheet" HREF='ja\v\as\cr\ipt:alert("XSS");'>
+EOT;
+		$error_slugs = $this->runCheck( $file_contents );
+		$this->assertContains( 'xss-in-link-tag-href', $error_slugs );
+	}
+
+	public function test_xss_not_in_link_tag_href() {
+		$file_contents = <<<'EOT'
+			<LINK REL="styleheet" HREF='stylesheet.css'>
+EOT;
+		$error_slugs = $this->runCheck( $file_contents );
+		$this->assertNotContains( 'xss-in-link-tag-href', $error_slugs );
+	}
+
+	public function test_xss_in_tag_style_attr() {
+		$file_contents = <<<'EOT'
+			<DIV STYLE="background-image: url(&#1;javascript:alert('XSS'))">
+EOT;
+		$error_slugs = $this->runCheck( $file_contents );
+		$this->assertContains( 'xss-in-style-attribute', $error_slugs );
+	}
+
+	public function test_xss_not_in_tag_style_attr() {
+		$file_contents = <<<'EOT'
+			<DIV STYLE="background-image: url(bg.gif)">
+EOT;
+		$error_slugs = $this->runCheck( $file_contents );
+		$this->assertNotContains( 'xss-in-style-attribute', $error_slugs );
+	}
+
+	public function test_moz_binding_in_tag_style() {
+		$file_contents = <<<'EOT'
+			<style>body{-moz-binding:url(http://ha.ckers.org/xssmoz.xml#xss)}</style>
+EOT;
+		$error_slugs = $this->runCheck( $file_contents );
+		$this->assertContains( 'moz-binding-xss-in-style-tag', $error_slugs );
+	}
+
+	public function test_moz_binding_not_in_tag_style() {
+		$file_contents = <<<'EOT'
+			<style>body{ color: #000; }</style>
+EOT;
+		$error_slugs = $this->runCheck( $file_contents );
+		$this->assertNotContains( 'moz-binding-xss-in-style-tag', $error_slugs );
+	}
+
+}
