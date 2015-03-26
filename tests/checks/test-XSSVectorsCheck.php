9e3a6784c3b25df581732001a4955a50cb94df27
Derrick Tennant <derrick@decarbonated.org>
2015-03-26 14:06:46 -0400

    VIP Scanner: Added comments to XSS unit tests.

--- tests/checks/test-XSSVectorsCheck.php
+++ tests/checks/test-XSSVectorsCheck.php
@@ -2,6 +2,9 @@
 require_once( 'CheckTestBase.php' );
 class XSSVectorsTest extends CheckTestBase {
 
+	/*
+	 * XSS javascript in link href tag
+	 */
 	public function test_xss_in_link_tag_href() {
 		$file_contents = <<<'EOT'
 			<LINK REL="styleheet" HREF='javascript:alert("XSS");'>
@@ -26,6 +29,9 @@ EOT;
 		$this->assertNotContains( 'xss-in-link-tag-href', $error_slugs );
 	}
 
+	/*
+	 * XSS javascript in style attribute and style tag
+	 */
 	public function test_xss_in_tag_style_attr() {
 		$file_contents = <<<'EOT'
 			<DIV STYLE="background-image: url(&#1;javascript:alert('XSS'))">
@@ -42,6 +48,25 @@ EOT;
 		$this->assertNotContains( 'xss-in-style-attribute', $error_slugs );
 	}
 
+	public function test_xss_in_style_tag() {
+		$file_contents = <<<'EOT'
+			<STYLE>@im\port'\ja\vasc\ript:alert("XSS")';</STYLE>
+EOT;
+		$error_slugs = $this->runCheck( $file_contents );
+		$this->assertContains( 'xss-javascript-in-style-tag', $error_slugs );
+	}
+
+	public function test_xss_not_in_style_tag() {
+		$file_contents = <<<'EOT'
+			<STYLE>body{ color: #000; }</STYLE>
+EOT;
+		$error_slugs = $this->runCheck( $file_contents );
+		$this->assertNotContains( 'xss-javascript-in-style-tag', $error_slugs );
+	}
+
+	/*
+	 * XSS -moz-binding in style attributes and style tag
+	 */
 	public function test_moz_binding_in_tag_style_attr() {
 		$file_contents = <<<'EOT'
 			<DIV STYLE="-moz-binding:url(http://ha.ckers.org/xssmoz.xml#xss)">
@@ -74,22 +99,9 @@ EOT;
 		$this->assertNotContains( 'moz-binding-xss-in-style-tag', $error_slugs );
 	}
 
-	public function test_xss_in_style_tag() {
-		$file_contents = <<<'EOT'
-			<STYLE>@im\port'\ja\vasc\ript:alert("XSS")';</STYLE>
-EOT;
-		$error_slugs = $this->runCheck( $file_contents );
-		$this->assertContains( 'xss-javascript-in-style-tag', $error_slugs );
-	}
-
-	public function test_xss_not_in_style_tag() {
-		$file_contents = <<<'EOT'
-			<STYLE>body{ color: #000; }</STYLE>
-EOT;
-		$error_slugs = $this->runCheck( $file_contents );
-		$this->assertNotContains( 'xss-javascript-in-style-tag', $error_slugs );
-	}
-
+	/*
+	 * XSS CSS expression property in style attribute and style tag
+	 */
 	public function test_css_expression_xss_in_style_tag() {
 		$file_contents = <<<'EOT'
 			<STYLE>body{ width: expression(alert('XSS')); }</STYLE>
