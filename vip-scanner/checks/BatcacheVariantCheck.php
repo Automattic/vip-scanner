eead54cd0c50de8f3bd149f3bfe9597df83cdc4a
Devon Liu <devon.liu@gmail.com>
2015-05-27 14:05:00 -0400

    Improve error descriptions on BatcacheVariantCheck
    Improve error descriptions on batcache variant error:
    - indicate the illegal word that causes the warning
    - mention superglobal that is missing

--- vip-scanner/checks/BatcacheVariantCheck.php
+++ vip-scanner/checks/BatcacheVariantCheck.php
@@ -1,10 +1,8 @@
 <?php
-/**
+/*
  * Checks for invalid batcache variant function vary_cache_on_function() that:
- * 
  * - includes dangerous illegal functions
  * - does not reference superglobal variable
- *
  */
 
 class BatcacheVariantCheck extends CodeCheck {
@@ -14,25 +12,20 @@ class BatcacheVariantCheck extends CodeCheck {
 			'PhpParser\Node\Expr\FuncCall' => function( $node ) {
 				$name = $node->name->toString();
 				if ( 'vary_cache_on_function' == $name ) {
-
 					$value = $node->args[0]->value;
-
 					if ( $value instanceof PhpParser\Node\Scalar\String )  {
-
 						$function_string = $value->value;
-
-						if ( preg_match('/include|require|echo|print|dump|export|open|sock|unlink|`|eval/i', $function_string) ) {
+						if ( preg_match('/include|require|echo|print|dump|export|open|sock|unlink|`|eval/i', $function_string, $matches) ) {
 							$this->add_error(
 								'batcache-variant-error',
-								'Illegal word in variant determiner.',
+								'Illegal word "'.$matches[0].'" in variant determiner.',
 								BaseScanner::LEVEL_BLOCKER
 							);
 						}
-
 						if ( !preg_match('/\$_/', $function_string) ) {
 							$this->add_error(
 								'batcache-variant-error',
-								'Variant determiner should refer to at least one $_ variable.',
+								'Variant determiner should refer to at least one $_ superglobals.',
 								BaseScanner::LEVEL_BLOCKER
 							);
 						}
@@ -41,6 +34,4 @@ class BatcacheVariantCheck extends CodeCheck {
 			}
 		) );
 	}
-
-
 }
