2ab8d369a47aebffbda3c5441aab5bf3b7cc67b0
Devon Liu <devon.liu@gmail.com>
2015-05-28 03:19:27 -0400

    Add positive test case and flexible assertion
    - Assert only on the slug part from the error, so description can be
    flexible
    - Separate test files into two to allow for two separate testings
    - Remove "assertFalse" from CodeCheckTestBase.php to allow for testing
    of positive test cases

--- tests/checks/test-BatcacheVariantCheck.php
+++ tests/checks/test-BatcacheVariantCheck.php
@@ -5,24 +5,15 @@ require_once( 'CodeCheckTestBase.php' );
 class BatcacheVariantTest extends CodeCheckTestBase {
 
 	public function testBatcacheVariant() {
-		$expected_errors = array(
-			array(
-				'slug' => 'batcache-variant-error',
-				'level' => BaseScanner::LEVEL_BLOCKER,
-				'description' => 'Illegal word in variant determiner.',
-				'file' => 'BatcacheVariantTest.inc',
-				'lines' => 7,
-			),
-			array(
-				'slug' => 'batcache-variant-error',
-				'level' => BaseScanner::LEVEL_BLOCKER,
-				'description' => 'Variant determiner should refer to at least one $_ variable.',
-				'file' => 'BatcacheVariantTest.inc',
-				'lines' => 7,
-			),
-		);
-		$actual_errors = $this->checkFile( 'BatcacheVariantTest.inc' );
-		$this->assertEqualErrors( $expected_errors, $actual_errors );
+		$actual_errors = $this->checkFile( 'BatcacheVariantTest1.inc' );
+		$error_slug = wp_list_pluck( $actual_errors, 'slug' );
+		$this->assertContains( 'batcache-variant-error', $error_slug );
+	}
+
+	public function testBatcacheVariantPositive() {
+		$actual_errors = $this->checkFile( 'BatcacheVariantTest2.inc' );
+		$error_slug = wp_list_pluck( $actual_errors, 'slug' );
+		$this->assertNotContains( 'batcache-variant-error', $error_slug );
 	}
 
 }
